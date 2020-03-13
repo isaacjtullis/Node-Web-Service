43ffc64426992e65b5b0f36d2a85b81e44f92a89
Isaac Tullis <isaac@verticalmerger.com>
2020-03-13 10:47:59 +0800

    Connected app to mongoose db

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+
+const Book = require('./Models/bookModel');
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    Book.find((err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
+  });
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to my API!');
---
+++ booksJson.js
@@ -0,0 +1,14 @@
+db.books.insert([
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolay',
+    read: false
+  },
+  {
+    title: 'Lord of the Rings',
+    genre: 'Fantasy',
+    author: 'J.R.R. Tolkien',
+    read: true
+  }
+]);
