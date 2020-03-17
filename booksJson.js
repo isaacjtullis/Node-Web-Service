669b0bdaff069271d68aaa9e9ab2f10230488395
Isaac Tullis <isaac@verticalmerger.com>
2020-03-18 01:06:28 +0800

    Create api endpoint to get single item from books

--- booksJson.js
+++ booksJson.js
@@ -10,5 +10,41 @@ db.books.insert([
     genre: 'Fantasy',
     author: 'J.R.R. Tolkien',
     read: true
+  },
+  {
+    title: 'Prince Caspian',
+    genre: 'Fantasy',
+    author: 'C.S. Lewis',
+    read: false
+  },
+  {
+    title: 'Count of Monte Cristo',
+    genre: 'Classics',
+    author: 'Victor Hugo',
+    read: true
+  },
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolay',
+    read: false
+  },
+  {
+    title: 'Lord of the Rings',
+    genre: 'Classics',
+    author: 'J.R.R. Tolkien',
+    read: true
+  },
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolay',
+    read: false
+  },
+  {
+    title: 'Lord of the Rings',
+    genre: 'Classics',
+    author: 'J.R.R. Tolkien',
+    read: true
   }
 ]);
